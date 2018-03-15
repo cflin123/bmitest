2c72548fc1b670c1d3d44c44be527e15aac0678c
cflin123 <cflin@im.cjcu.edu.tw>
2018-03-15 16:09:20 +0800

    FFF

---
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.cjcu.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText ed_weight;
+    private EditText ed_height;
+    private Button btn_help;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+
+
+
+
+    }
+
+    private void findViews() {
+        ed_weight = findViewById(R.id.ed_weight);
+        ed_height = findViewById(R.id.ed_height);
+
+        btn_help = findViewById(R.id.btn_help);
+    }
+
+
+    public void bmi(View view) {
+        float w = Float.parseFloat(ed_weight.getText().toString());
+        float h = Float.parseFloat(ed_height.getText().toString());
+        float bmi = w / (h*h);
+        Log.d("BMI",String.valueOf(bmi));
+
+
+
+    }
+}
